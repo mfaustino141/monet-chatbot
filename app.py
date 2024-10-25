3bd62438400f783596e1032c9fafca73b8f5693c
mfaustino141 <maggie.faustino141@gmail.com>
2024-10-25 16:52:26 -0400

    Hopefully this works

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from gradio_client import Client
 
@@ -43,4 +44,5 @@ def chat():
     return {"response": response}
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
